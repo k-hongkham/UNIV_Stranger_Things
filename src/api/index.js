7c8d03c31601d523cf3327b71ca503a217b135b0
k-hongkham <k.hongkham@gmail.com>
2022-03-13 22:42:39 -0400

    posts are now appearing.

--- src/api/index.js
+++ src/api/index.js
@@ -13,13 +13,16 @@ export const registerUser = async (username, password) => {
   console.log(data);
   return data;
 };
-export function fetchAllPosts = async () => {
-  try {
-    const response = fetch(`{base_url/posts}`);
-    const result = await response.json();
-    if(result.error) throw result.error;
-    return result.data.posts;
-  } catch (err) {
-    console.error("ERROR: fetchAllPosts", err)
-  }
-};
+
+// export const fetchAllPosts = async () => {
+//   const response = await fetch(`${base_url}/posts`, {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({}),
+//   });
+//   const data = await response.json();
+//   console.log(data);
+//   return data;
+// };
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { fetchAllPlayers } from "../api";
+// import { fetchAllPosts } from "../api";
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
@@ -9,10 +9,10 @@ const Posts = () => {
       const response = await fetch(
         "https://strangers-things.herokuapp.com/api/2202-ftb-et-web-ft/posts"
       );
-
       const data = await response.json();
+      console.log("data:", data);
 
-      setPosts(data);
+      setPosts(data.data.posts);
     };
     fetchPostsView();
   }, []);
@@ -20,9 +20,9 @@ const Posts = () => {
   return (
     <>
       <h1>Posts</h1>
-      {data.posts.map((post) => (
-        <div key={data.post.id}>{data.post.title}</div>
-      ))}
+      {posts.map((post, idx) => {
+        return <div key={post._id}>{post.title}</div>;
+      })}
     </>
   );
 };
