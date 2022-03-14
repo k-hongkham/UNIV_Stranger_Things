e8ff65e311c7d36a66f0f1d9316029d64a3fe1ac
KanishaMckibben <kj165299@LAPTOP-BLBN3LE9.localdomain>
2022-03-14 13:10:59 -0400

    added posts and in progress

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Posts from "./Posts"
+
 const Main = () => {
   return (
     <div>
       <h1>hello main</h1>
+    <Posts />
     </div>
   );
 };
---
+++ src/components/Posts.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { useEffect } from "react/cjs/react.production.min"
+
+const Posts =()=> {
+    const [Posts, setPosts]= useState([])
+
+    useEffect(()=>{
+
+    })
+    
+    return 
+}
+
+export default Posts
