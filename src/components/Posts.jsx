b83239c8979d3495867d0f9df302d93c7e850a74
KanishaMckibben <kj165299@LAPTOP-BLBN3LE9.localdomain>
2022-03-15 11:16:12 -0400

    import and export SinglePost

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { fetchAllPosts } from "../api";
+import SinglePost from "./SinglePost";
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
@@ -21,8 +22,7 @@ const Posts = () => {
       {posts.map((post, idx) => {
         return (
           <>
-            <h1>SHOULDWORK</h1>
-          </>
+<SinglePost />       </>
         );
       })}
     </>
