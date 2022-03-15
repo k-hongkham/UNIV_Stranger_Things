854f9a6afac92e2a9dde5e7b32019bd200469a6f
keefe089e <kelleybkeefer@googlemail.com>
2022-03-15 14:12:53 -0500

    set myTokena and setToken

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,13 +1,20 @@
 import React, {useState} from "react"
+import { registerUser } from "../api"
 
-const SignUp = ()=>{
+const SignUp = ({setToken})=>{
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
     return(
         <div>
-            <form>
+            <form onSubmit = {async(e)=>{
+                e.preventDefault()
+                const result = await registerUser(username, password)
+                localStorage.setItem("token", result.data.token);
+                const myToken = localStorage.setItem(result.data.token)
+                setToken(myToken)
+            }}>
                 <input type = "text" placeholder = "Username" value = {username} onChange= {(e)=>
     setUsername(e.target.value)}>
                 </input>
