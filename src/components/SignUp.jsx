fdcd8736e58471c4bf2ab87034636a197f945d2e
keefe089e <kelleybkeefer@googlemail.com>
2022-03-15 14:02:54 -0500

    set the username and password on the sign up page

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,17 +1,20 @@
-import React from "react"
+import React, {useState} from "react"
 
 const SignUp = ()=>{
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
 
     return(
         <div>
             <form>
                 <input type = "text" placeholder = "Username" value = {username} onChange= {(e)=>
-    setPostTitle(e.target.value)}>
+    setUsername(e.target.value)}>
                 </input>
                 <input  type = "text" placeholder = "Password" value = {password} onChange= {(e)=>
-    setPostTitle(e.target.value)}>
+    setPassword(e.target.value)}>
                 </input>
-                <button>
+                <button type= "submit"> Enter
 
                 </button>
             </form>
