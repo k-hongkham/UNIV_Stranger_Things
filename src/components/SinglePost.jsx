db3abfb355435914d554bf53903f9de610bab4df
keefe089e <kelleybkeefer@googlemail.com>
2022-03-15 12:32:18 -0500

    created template for update posts

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -5,7 +5,16 @@ const SinglePost = ()=> {
 
     return(
         <div>
-            
+            <h3>{post.title}</h3>
+<div>{post.description}</div>        
+<form onSubmit= {(e)=> {
+    handleSubmit(e, post._id)
+
+}}>
+    <input placeholder = "title" value = {postTitle} onChange= {(e)=>
+    setPostTitle(e.target.value)} />
+    <button type="submit">Update</button>
+</form>
         </div>
     )
 }
