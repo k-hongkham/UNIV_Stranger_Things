520af864a0d3d467577b49ea06df99e38ad469d7
k-hongkham <k.hongkham@gmail.com>
2022-03-15 10:58:27 -0400

    correcting index.js under api.

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const base_url = "https://strangers-things.herokuapp.com/api/2202-ftb-et-web-ft"
+const base_url =
+  "https://strangers-things.herokuapp.com/api/2202-ftb-et-web-ft";
 
-
-export const fetchAllPosts = async()=>{
-    const response = await fetch(`${base_url}/posts`);
-const data = response.json()
-return data} 
+export const fetchAllPosts = async () => {
+  const response = await fetch(`${base_url}/posts`);
+  const data = await response.json();
+  return data;
+};
