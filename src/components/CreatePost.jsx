868b9df17507f8e52bebee120ece62b9003c2799
KanishaMckibben <kj165299@LAPTOP-BLBN3LE9.localdomain>
2022-03-17 11:42:11 -0400

    defined Createpost function

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,12 +1,33 @@
-import React from "react"
+import React, { UseState } from "react";
 
 const CreatePost = () => {
+  const [title, setTitle] = UseState("");
+  const [details, setDetails] = UseState("");
 
-    return (
-        <div className = "CreatePostContainer">
-
-        </div>
-    )
-}
+  return (
+    <div className="CreatePostContainer">
+      <h3>CREATE</h3>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e, el._id);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="title"
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="details"
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}
+        />
+        <button type="submit">Enter</button>
+      </form>
+    </div>
+  );
+};
 
 export default CreatePost;
