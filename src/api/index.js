69b95df308fa1c7d900351a031e5417a7ffec111
k-hongkham <k.hongkham@gmail.com>
2022-03-18 20:06:29 -0400

    merging branches and confirming changes.

--- src/api/index.js
+++ src/api/index.js
@@ -27,7 +27,6 @@ export const registerUser = async (username, password) => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-<<<<<<< HEAD
     },
     body: JSON.stringify({
       user: {
@@ -45,8 +44,6 @@ export const Login = async (username, password) => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-=======
->>>>>>> 2125bd3711d27623dcb930ef732df9f40097e6a9
     },
     body: JSON.stringify({
       user: {
@@ -58,27 +55,14 @@ export const Login = async (username, password) => {
   const data = await response.json();
   return data;
 };
-<<<<<<< HEAD
-=======
 
-export const createPost = async (createdPost, token) => {
-  try {
-    const response = await fetch(`${base_url}//posts`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        post: createdPost,
-      }),
-    });
-    const data = await response.json();
-    if (data.success === false) {
-      throw new Error(data.error.message);
-    }
-  } catch (error) {
-    console.error(error);
-  }
+export const isMe = async (token) => {
+  const response = await fetch(`${base_url}/users/me`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+  return data;
 };
->>>>>>> 2125bd3711d27623dcb930ef732df9f40097e6a9
--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,5 @@ export { default as Main } from  "./Main";
 export { default as Posts } from  "./Posts";
 export {default as Navbar} from "./Navbar";
 export {default as SignUp} from "./SignUp";
-<<<<<<< HEAD
 export {default Login} from "./Login";
-=======
 export{default as CreatePost} from "./CreatePost";
->>>>>>> 2125bd3711d27623dcb930ef732df9f40097e6a9
