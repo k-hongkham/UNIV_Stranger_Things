7571fe7c50bbea14b81eaf90c763150d6921adc5
k-hongkham <k.hongkham@gmail.com>
2022-03-18 23:08:24 -0400

    trying to insert search bar into posts.

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { fetchAllPosts } from "../api";
 
-const Posts = ({ token, theUser, setTheUser }) => {
-  const [posts, setPosts] = useState([]);
-
+const Posts = ({ token, theUser, setTheUser, posts, setPosts }) => {
+  
   useEffect(() => {
     const getPosts = async () => {
       const response = await fetchAllPosts();
@@ -14,23 +13,51 @@ const Posts = ({ token, theUser, setTheUser }) => {
     getPosts();
   }, [token]);
 
+  const handleClick = async (post_id, token) => {
+    const response = await deletePost(post_id,window.localStorage.getItem("token") );
+    const resetPosts = [];
+    
+
+
+    posts.forEach((post) => {
+      if(post._id !== post_id) {
+      resetPosts.push(post)
+      }
+    })
+    setPosts(resetPosts)
+  }
   return (
-    <>
-      {posts.map((el, idx) => {
-        return (
-          <>
-            {/* <SinglePost
-              posts={posts}
-              setPosts={setPosts}
-              el={el}
-              theUser={theUser}
-              setTheUser={setTheUser}
-              key={idx}
-            />{" "} */}
-          </>
+    <div>
+      <form className="search_bar" onSubmit={(e) =>{
+        e.preventDefault();
+      }}>
+        <input
+          type="text"
+          placeholder="FILTER"
+          value={searchTerm}
+          onChange={(e) => {setSearchTerm(e.target.value)}}
+        ></input>
+        <button type="submit">FILTER</button>
+
+      </form>
+      
+    </div>
+   
+    <div className="post_container"
+      {posts.map((post, idx) => 
+
+<h3>{el.title}</h3>
+<h2>{el.author.username}</h2>
+<div>{el.description}</div>
+<div>{el.price}</div>
+<div>{el.location}</div>
+<div>{el.willDeliver}</div>
+
+          
         );
-      })}
-    </>
+        </div>
+      })
+   
   );
 };
 
